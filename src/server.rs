009409fe62cdc04976e16eae4450443d1998c178
Tyler Forshaw <tmforshaw@outlook.com>
2022-08-31 20:49:15 +0100

    Update every 1.5 seconds regardless of recieving an event

--- src/server.rs
+++ src/server.rs
@@ -4,11 +4,21 @@ use crate::error;
 use crate::memory;
 use crate::volume;
 
+use std::time::Duration;
+
 use tokio::io::AsyncReadExt;
 use tokio::net::TcpListener;
 
 pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    tokio::spawn(async move {
+        loop {
+            println!("{}", get_all_json());
+
+            std::thread::sleep(Duration::from_millis(1500));
+        }
+    });
 
     loop {
         let (mut socket, _) = listener.accept().await?;
@@ -38,16 +48,20 @@ pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
                     "brightness" => println!("{}", brightness::get_json()),
                     "battery" => println!("{}", battery::get_json()),
                     "memory" => println!("{}", memory::get_json()),
-                    "all" => println!(
-                        "{{\"volume\": {}, \"brightness\": {}, \"battery\": {}, \"memory\": {}}}",
-                        volume::get_json(),
-                        brightness::get_json(),
-                        battery::get_json(),
-                        memory::get_json()
-                    ),
-                    _ => println!("{message}"),
+                    "all" => println!("{}", get_all_json()),
+                    _ => {}
                 };
             }
         });
     }
+}
+
+fn get_all_json() -> String {
+    format!(
+        "{{\"volume\": {}, \"brightness\": {}, \"battery\": {}, \"memory\": {}}}",
+        volume::get_json(),
+        brightness::get_json(),
+        battery::get_json(),
+        memory::get_json()
+    )
 }
