9c1dd27b2a41da96b5b0f267ee3d5b2fb9079eb8
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-04 17:42:29 +0100

    Go black to using tlp bluetooth

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -7,7 +7,7 @@ pub struct Bluetooth {}
 
 impl Bluetooth {
     fn get_state() -> Result<bool, Arc<ServerError>> {
-        command::run("bluetoothctl", &["list"])?
+        command::run("bluetooth", &[])?
             .split_whitespace()
             .nth(2)
             .map_or_else(|| todo!(), |value| Ok(value == "on"))
