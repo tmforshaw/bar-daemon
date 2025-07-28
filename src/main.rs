80821308087fe92517d2928c9cf2d6e6b5a9a231
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-28 18:27:16 +0100

    Starting to add volume commands

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use crate::{cli::match_cli, error::DaemonError};
 pub mod cli;
 pub mod daemon;
 pub mod error;
+pub mod volume;
 
 #[tokio::main]
 async fn main() -> Result<(), DaemonError> {
---
+++ src/volume.rs
@@ -0,0 +1,10 @@
+use crate::error::DaemonError;
+
+pub struct Volume;
+
+impl Volume {
+    // TODO
+    fn get() -> Result<String, DaemonError> {
+        Ok(String::new())
+    }
+}
