589a2bbae13eafcc6e783bbc1ea2b4dbe5811e37
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-28 17:08:28 +0100

    Parse get and set commands

--- src/daemon.rs
+++ src/daemon.rs
@@ -9,14 +9,19 @@ use crate::error::DaemonError;
 pub const IP_AND_PORT: &str = "127.0.0.1:6969";
 pub const BUFFER_SIZE: usize = 1024;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum DaemonMessage {
     Set { item: DaemonItem, value: String },
     Get { item: DaemonItem },
-    Reply { item: DaemonItem, value: String },
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum DaemonReply {
+    Value { item: DaemonItem, value: String },
+    Error(String),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum DaemonItem {
     Volume,
 }
@@ -45,21 +50,8 @@ pub async fn do_daemon() -> Result<(), DaemonError> {
                 println!("{message:?}");
 
                 let reply = match message {
-                    DaemonMessage::Set { item, value } => {
-                        // TODO Set the item
-                        DaemonMessage::Reply { item, value }
-                    }
-                    DaemonMessage::Get { item } => {
-                        // TODO Get the value of this item
-                        DaemonMessage::Reply {
-                            item,
-                            value: "This is the value of your gotten thing".to_string(),
-                        }
-                    }
-                    DaemonMessage::Reply { item: _, value: _ } => {
-                        // Should not reach this
-                        unreachable!()
-                    }
+                    DaemonMessage::Set { item, value } => match_set_command(item.clone(), value.clone()).await,
+                    DaemonMessage::Get { item } => match_get_command(item.clone()).await,
                 };
 
                 // Send the reply back
@@ -72,7 +64,7 @@ pub async fn do_daemon() -> Result<(), DaemonError> {
     }
 }
 
-pub async fn send_daemon_messaage(message: DaemonMessage) -> Result<DaemonMessage, DaemonError> {
+pub async fn send_daemon_messaage(message: DaemonMessage) -> Result<DaemonReply, DaemonError> {
     // Connect to the daemon
     let mut stream = TcpStream::connect(IP_AND_PORT).await?;
 
@@ -85,4 +77,25 @@ pub async fn send_daemon_messaage(message: DaemonMessage) -> Result<DaemonMessag
 
     // Serialize reply into DaemonMessage
     Ok(postcard::from_bytes(&buf[..n])?)
+}
+
+pub async fn match_set_command(item: DaemonItem, value: String) -> DaemonReply {
+    match item {
+        DaemonItem::Volume => println!("Set Volume {value}"),
+    }
+
+    // TODO
+    DaemonReply::Value { item, value }
+}
+
+pub async fn match_get_command(item: DaemonItem) -> DaemonReply {
+    match item {
+        DaemonItem::Volume => println!("Get Volume"),
+    }
+
+    // TODO
+    DaemonReply::Value {
+        item,
+        value: "The value you have gotten".to_string(),
+    }
 }
